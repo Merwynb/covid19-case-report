902fb62b325888d2bf6f9c1770e69d8ace08c4fc
MERWYN BARRETTO <merwynbarretto@gmail.com>
2022-08-16 22:42:59 +0000

    Create function update_new_cases_column to update New cases column in worksheet

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
-    ]
+]
 
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
@@ -17,27 +17,49 @@ def get_new_cases():
     """
     Get New cases data from user.
     """
-    print("Please enter total number of New cases")
-    print("Value should be in numerical format. Example: 33")
-    print("Data should be entered ONLY at the end of day i.e. at 22:00\n")
+    while True:
+        print("Please enter total number of New cases.")
+        print("Value should be in numerical format. Example: 33")
+        print("Data should be entered ONLY at the end of day i.e. at 22:00\n")
 
-    new_cases = input("Enter your data here: ")
-    print(f"data entered is {new_cases} ")
+        data_str = input("Enter New Cases: ")
 
-    validate_data(new_cases)
+        new_cases_data = data_str.split(" ")
+        
+        if validate_data(new_cases_data):
+            print("Thank You for your input.\n")
+            break
+
+    return new_cases_data    
 
 
-def validate_data(new_cases):
+def validate_data(values):
     """
-    Checks to confirm user enters an Integer 
-    and will keep asking until user enters a valid input.
+    converts string into integers and checks to confirm 
+    user enters an Integer and will keep asking until 
+    user enters a valid input.
     """
     try:
-        user_num = int(new_cases)
-        return user_num
+        [int(value) for value in values]
+        if len(values) > 1:
+            raise ValueError()
     except ValueError:
-        print("Please enter a numerical value.\n")
-        return get_new_cases()
+        print("Invalid input. Please enter a numerical value.\n")
+        return False
+
+    return True    
 
 
-get_new_cases()
+def update_new_cases_column(new_cases):
+    """
+    Updates the daily worksheet and adds a new row with the data.
+    """
+    print("Updating Daily worksheet...\n")
+    new_cases_column = SHEET.worksheet("daily")
+    new_cases_column.append_row(new_cases)
+    print("Daily worksheet updated successfully.\n")
+
+
+data = get_new_cases()
+new_cases_data = [int(num) for num in data]
+update_new_cases_column(new_cases_data)
